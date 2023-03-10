f1749852cdf7eb39c060a8cc1d51171e4fe15285
Johnny14312 <johnny.zeng@baruchmail.cuny.edu>
2023-03-09 19:07:39 -0500

    first commit

---
+++ scAdademy/force-app/main/default/lwc/newLWC/newLWC.js
@@ -0,0 +1,76 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import sendEmail from '@salesforce/apex/newLWCController.sendEmail';
+
+export default class NewLWC extends LightningElement {
+
+
+   toAddress; //pretty much same as String myString = new String();
+   subject;
+   body;
+
+   
+    //public static void handleSendButtonClick() } // }
+   handleSendButtonClick() {
+        sendEmail({
+            toAddress : this.toAddress,
+            subject : this.subject,
+            body : this.body
+        }).then(result => {
+            this.showNoticiation();
+        }).catch(error => {
+
+        })
+   }
+
+   handleInputChange(event) {
+       var componentName = event.target.name;
+      
+       //CHANGE TO A SWITCH CASE
+       /*
+       if (componentName == 'toAddress') {
+           this.toAddress = event.detail.value;
+       } else if (componentName == 'subject') {
+           this.subject = event.detail.value;
+       } else if (componentName == 'body') {
+           this.body = event.detail.value;
+       }*/
+
+
+       switch (componentName) {
+           case 'toAddress':
+               this.toAddress = event.detail.value;
+           case 'subject':
+               this.subject = event.detail.value;
+           case 'body':
+               this.body = event.detail.value;
+       }
+   }
+
+
+
+
+   showNotification() {
+       var toastEvent = new ShowToastEvent({
+           title: 'Hooray!',
+           message: 'Message successfully sent',
+           variant: 'success',
+       });
+       this.dispatchEvent(toastEvent);
+   }
+
+
+
+
+
+
+   /*
+       console.log('IM DEBUGGING!');
+       console.log('TO ADDRESS');
+       console.log(this.toAddress);
+       console.log('Subject');
+       console.log(this.subject);
+       console.log('Body');
+       console.log(this.body);
+   */
+}
